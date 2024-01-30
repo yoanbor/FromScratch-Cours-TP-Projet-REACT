7acefff50fe7d9feeceddd3f0db1c5831941ed84
Ylorklan <yoanbor@icloud.com>
2024-01-30 13:18:57 +0100

    Envoie de mes premiers cours

---
+++ COURS/Cours React 2:2/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
